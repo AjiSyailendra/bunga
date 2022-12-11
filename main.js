bf61eb61d14abfe46b524e47fc96ca1aa72f6618
Syailendra31 <ajisyailendra.office@gmail.com>
2022-12-11 18:37:45 +0700

    finishing

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
-onload = () => {
-        const c = setTimeout(() => {
-                document.body.classList.remove("not-loaded");
-                clearTimeout(c);
-        }, 5000);
-};
+// var x = document.getElementById("myAudio");
 
-var x = document.getElementById("myAudio");
 
 // Swal.fire({
-//         title: "SIAPP?",
-//         text: "Klik OK jika siap yak",
+//         title: "SURPRISEE",
+//         timer: 500,
 //         imageUrl: "./assets/pentolawal.gif",
-//         imageHeight: 200,
+//         imageWidth: 250,
+//         imageHeight: 250,
+//         timerProgressBar: true,
+//         showConfirmButton: false,
 // }).then((res) => {
 //         if (res.isConfirmed) {
-//                 document.body.classList.remove("not-loaded");
-//                 x.play();
+//                 x.play()
 //         }
 // })
 
+var x = document.getElementById("myAudio");
+
+onload = () => {
+        Swal.fire({
+                title: "SIAPP?",
+                timer: 500,
+                text: "Klik OK jika siap yak",
+                imageUrl: "./assets/pentolawal.gif",
+                imageHeight: 200,
+                // showConfirmButton: false
+        }).then((res) => {
+                res.isConfirmed = true
+                if (res.isConfirmed) {
+                        x.play();
+                }
+        })
+}
--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var x = document.getElementById("myAudio");
 
-Swal.fire({
-  title: "SIAPP?",
-  text: "Klik OK jika siap yak",
-  imageUrl: "./assets/pentolawal.gif",
-  imageHeight: 200,
-}).then((res) => {
-  if (res.isConfirmed) {
-    document.body.classList.remove("not-loaded");
-    x.play();
-  }
-})
+onload = () => {
+  Swal.fire({
+    title: "SIAPP?",
+    text: "Klik OK jika siap yak",
+    imageUrl: "./assets/pentolawal.gif",
+    imageHeight: 200,
+  }).then((res) => {
+    if (res.isConfirmed) {
+      document.body.classList.remove("not-loaded");
+      x.play();
+    }
+  })
+}
